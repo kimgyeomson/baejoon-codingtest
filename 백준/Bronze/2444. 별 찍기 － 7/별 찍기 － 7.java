7edccbe3fbae056821e9b7c539df82cff879274d
kimgyeomson <122075249+kimgyeomson@users.noreply.github.com>
2024-04-14 19:11:10 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 136 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n-i; j++) {
+                sb.append(" ");
+            }
+            for(int j = 1; j <= 2*i-1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = 1; j <= n-i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= 2*i-1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+
+
+
+    }
+}
